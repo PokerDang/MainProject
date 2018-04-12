61a2ad3bc7fe44976496bc07bb86513d45dab75f
PokerDang <timothydyc@gmail.com>
2018-04-13 07:33:44 +0800

    2018/04/13 程序更新：更新Report

--- DailyReport/Report.py
+++ DailyReport/Report.py
@@ -35,7 +35,8 @@ dataweek = pd.DataFrame(w.wss(index, "pct_chg_per",weekperiod).Data,columns=name
 datamonth = pd.DataFrame(w.wss(index, "pct_chg_per",monthperiod).Data,columns=name,index=[u'月涨跌幅']).T
 datayear = pd.DataFrame(w.wss(index, "pct_chg_per",yearperiod).Data,columns=name,index=[u'年涨跌幅']).T
 
-datalatestprice = pd.DataFrame(w.wsd(index, "close", lasttradedate,lasttradedate, "Fill=Previous").Data,index=[u'最新价'],columns=name).T
+datalatestprice = pd.DataFrame(w.wsd(index, "close", lasttradedate,lasttradedate,\
+                                     "Fill=Previous").Data,index=[u'最新价'],columns=name).T
 
 report = pd.concat([datalatestprice,dataday,dataweek,datamonth,datayear],axis=1,join_axes=[datalatestprice.index])#合并
 report.to_excel('report'+'_'+lasttradedate+'.xlsx',sheet_name='Sheet1')
@@ -47,15 +48,18 @@ print report,'\n\n'
 #Future
 IHFuture = "IH1804.CFE,IH1805.CFE,IH1806.CFE,IH1809.CFE"
 IHName = ['IH1804','IH1805','IH1806','IH1809']
-IHlatestprice = pd.DataFrame(w.wsd(IHFuture, "close", lasttradedate,lasttradedate, "Fill=Previous").Data,index=[u'最新价'],columns=IHName).T
+IHlatestprice = pd.DataFrame(w.wsd(IHFuture, "close", lasttradedate,lasttradedate,\
+                                   "Fill=Previous").Data,index=[u'最新价'],columns=IHName).T
 
 IFFuture = "IF1804.CFE,IF1805.CFE,IF1806.CFE,IF1809.CFE"
 IFName = ['IF1804','IF1805','IF1806','IF1809']
-IFlatestprice = pd.DataFrame(w.wsd(IFFuture, "close", lasttradedate,lasttradedate, "Fill=Previous").Data,index=[u'最新价'],columns=IFName).T
+IFlatestprice = pd.DataFrame(w.wsd(IFFuture, "close", lasttradedate,lasttradedate, \
+                                   "Fill=Previous").Data,index=[u'最新价'],columns=IFName).T
 
 ICFuture = "IC1804.CFE,IC1805.CFE,IC1806.CFE,IC1809.CFE"
 ICName = ['IC1804','IC1805','IC1806','IC1809']
-IClatestprice = pd.DataFrame(w.wsd(ICFuture, "close", lasttradedate,lasttradedate, "Fill=Previous").Data,index=[u'最新价'],columns=ICName).T
+IClatestprice = pd.DataFrame(w.wsd(ICFuture, "close", lasttradedate,lasttradedate,\
+                                   "Fill=Previous").Data,index=[u'最新价'],columns=ICName).T
 
 print IHlatestprice,'\n',IFlatestprice,'\n',IClatestprice
 
@@ -64,5 +68,14 @@ print IHlatestprice,'\n',IFlatestprice,'\n',IClatestprice
 
 #Option
 
-50ETFOptionDaily =
 
+
+#Divident
+
+#分红实施情况
+DividentSZ50 = w.wset("dividendproposal","ordertype=1;startdate="+lastyeardate+\
+                       ";enddate="+lasttradedate+";sectorid=1000000087000000")#上证50
+DividentHS300 = w.wset("dividendproposal","ordertype=1;startdate="+lastyeardate+\
+                       ";enddate="+lasttradedate+";sectorid=1000000090000000")#沪深300
+DividentZZ500 = w.wset("dividendproposal","ordertype=1;startdate="+lastyeardate+\
+                       ";enddate="+lasttradedate+";sectorid=1000008491000000")#ZZ500
